994bcec676ffb9872ef12d222725585d06ae9223
Pouch-dev <diemphuoc5555@gmail.com>
2021-09-28 19:47:30 +0700

    diemphuoc_update_28/09/2021

---
+++ J5VA/src/test/java/com/example/J5VA/J5VaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.J5VA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J5VaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
