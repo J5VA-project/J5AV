86577d318dc71899894cd63abed61880567d2e2c
Pouch-dev <diemphuoc5555@gmail.com>
2021-10-08 22:18:38 +0700

    diemphuoc_update_08/10/2021

--- src/main/java/com/J5VA/controller/RegisterController.java
+++ src/main/java/com/J5VA/controller/RegisterController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.J5VA.entity.customer;
 import com.J5VA.service.MailerService;
@@ -38,21 +39,19 @@ public class RegisterController {
 	}
 
 	@PostMapping("/register/validate")
-	public String registerValid(Model model, @Valid @ModelAttribute("custo") @RequestBody customer custo, Errors errors)
+	public String registerValid(Model model, @Valid @ModelAttribute("custo") @RequestBody customer custo,@RequestParam("fullname")String fullname, Errors errors)
 			throws MessagingException {
-				String username = paramService.getString("name", "");
+				String username = paramService.getString("username_custo", "");
 				String password = paramService.getString("password", "");
 				String confirm = paramService.getString("confirmPass", "");
 				String email = paramService.getString("email", "");
-				System.out.println(username);	
-
 		if (!confirm.equals(password)) {
 			model.addAttribute("message", "Confirm password doesn't match!");
 			return "forward:/home/register";
 		} else {
 			model.addAttribute("custo", custo);
 			customerService.create(custo);
-			mailer.send(email, "Account Register", "Username: " + username + " Password: " + password);
+			mailer.send(email, "Account Register","Dear. "+fullname+ ", Wellcome to J5VA Restaurant, hope you like my website, and then my username: "+ username+ ", password: " + password);
 			model.addAttribute("message", "Register success!");
 			return "forward:/home/register";
 		}
