8b4212a62d7637cefe20000e1eae23dea58fd988
vuongnmudpm <vuongnmudpm@gmail.com>
2021-11-29 21:54:29 +0700

    vuongnm

--- src/main/java/com/J5VA/service/AuthorizedService.java
+++
@@ -1,9 +0,0 @@
-package com.J5VA.service;
-
-import java.util.List;
-
-import com.J5VA.entity.Authorized;
-
-public interface AuthorizedService {
-	List<Authorized> findAll();
-}
--- src/main/java/com/J5VA/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.J5VA.service;
-
-import java.util.List;
-
-import com.J5VA.entity.Role;
-
-public interface RoleService {
-	List<Role> findAll();
-}
