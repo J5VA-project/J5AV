4c95be9a748368d7bba7a164fc6e56ad8b509abd
dathx <dathxps12656@fpt.edu.vn>
2021-12-11 22:15:42 +0700

    fixed insert product

--- src/main/java/com/J5VA/entity/FoodDetail.java
+++ src/main/java/com/J5VA/entity/FoodDetail.java
@@ -7,7 +7,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
